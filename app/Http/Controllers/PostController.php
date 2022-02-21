4a0edc71aa0e89a1474731c3692eec28b21992f6
MaiiEmad <eng.maii.emad@gmail.com>
2022-02-21 16:20:17 +0200

    third commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -36,7 +36,7 @@ class PostController extends Controller
                
         Post::create($requestData);
         return to_route('posts.index');
-        //maii 
+    
     }
     public function show($postID)
     {
